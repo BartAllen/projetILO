376d1e83a031eb7cf17b801269a49f3250126af4
Adrian <braciglianoadrian@gmail.com>
2012-01-02 15:54:39 +0100

    Eclipse

---
+++ Composant/src/operateurBinaire.java
@@ -0,0 +1,19 @@
+
+public class operateurBinaire extends Composant {
+	
+	public double multiplication(double x, double y) {
+		return x * y;
+	}
+	
+	public double division(double x, double y) {
+		return x / y;
+	}
+	
+	public double addition(double x, double y){
+		return x + y;
+	}
+	
+	public double soustraction(double x, double y){
+		return x - y;
+	}
+}
